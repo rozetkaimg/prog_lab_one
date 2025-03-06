fbcdd982eb49159ba53c7f9c419af3cdd662b511
Artem <artemkap05@mail.ru>
2025-03-07 01:38:26 +0700

    my final

--- main.cpp
+++ main.cpp
@@ -1,18 +1,20 @@
 #include "circle.h"
+#include "rectangle.h"
 #include <iostream>
 #include <iomanip>
-#include "rectangle.h"
 
 using namespace std;
 
-enum class ShapeOption {
+enum class ShapeOption
+{
     Circle = 1,
     Rectangle,
     Trapezoid,
-    Exit
+    Exit = 0
 };
 
-enum RectFunc {
+enum RectFunc
+{
     GetHeight = 1,
     GetWidth,
     GetPerimeter,
@@ -22,21 +24,21 @@ enum RectFunc {
     SetWidth,
 };
 
-void mainCircleFunction() {
-    std::cout << "Введите радиус: " << std::endl;
+void mainCircleFunction()
+{
+    cout << "Введите радиус: " << endl;
     int input_radius = 0;
 
-
-    while (!(std::cin >> input_radius) || input_radius < 0) {
-        std::cin.clear();
-        std::cin.ignore(10000, '\n');
-        std::cout << "Ошибка! Введите корректное значение радиуса (неотрицательное число): " << std::endl;
+    while (!(cin >> input_radius) || input_radius < 0)
+    {
+        cin.clear();
+        cin.ignore(10000, '\n');
+        cout << "Ошибка! Введите корректное значение радиуса (неотрицательное число): " << endl;
     }
 
     Circle myCircle(input_radius);
     myCircle.processChoice(myCircle);
 }
-
 
 void RectOption()
 {
@@ -45,85 +47,97 @@ void RectOption()
     while (true)
     {
         cout << "Выберите, что вывести:\n";
-        cout << "1 - Высоту\n2 - Ширину\n3 - Периметр\n4 - Диагональ\n5 - Площадь\n6 - Ввести Высоту\n7 - Ввести Ширину\n0 - Выйти" << endl;
+        cout <<
+            "1 - Высоту\n2 - Ширину\n3 - Периметр\n4 - Диагональ\n5 - Площадь\n6 - Ввести Высоту\n7 - Ввести Ширину\n0 - Выйти"
+            << endl;
         cin >> choice;
         switch (choice)
         {
-            case GetHeight:
-                cout << "Высота: " << myRectangle.getHeight() << endl;
+        case GetHeight:
+            cout << "Высота: " << myRectangle.getHeight() << endl;
             break;
-            case GetWidth:
-                cout << "Ширина: " << myRectangle.getWidth() << endl;
+        case GetWidth:
+            cout << "Ширина: " << myRectangle.getWidth() << endl;
             break;
-            case GetPerimeter:
-                cout << "Периметр: " << myRectangle.getPerimeter() << endl;
+        case GetPerimeter:
+            cout << "Периметр: " << myRectangle.getPerimeter() << endl;
             break;
-            case GetDiagonal:
-                cout << "Диагональ: " << myRectangle.getDiagonal() << endl;
+        case GetDiagonal:
+            cout << "Диагональ: " << myRectangle.getDiagonal() << endl;
             break;
-            case GetArea:
-                cout << "Площадь: " << myRectangle.getArea() << std::endl;
+        case GetArea:
+            cout << "Площадь: " << myRectangle.getArea() << endl;
             break;
-            case SetHeight:
-                int newHeight;
+        case SetHeight:
+            int newHeight;
             cout << "Введите новую высоту: ";
             cin >> newHeight;
+            if (newHeight < 0)
+            {
+                cout << "Высота не может быть отрицательной. Попробуйте снова." << endl;
+                break;
+            }
             myRectangle.setHeight(newHeight);
-            cout << "Высота изменена на " << myRectangle.getHeight() << std::endl;
+            cout << "Высота изменена на " << myRectangle.getHeight() << endl;
             break;
-            case SetWidth:
-                int newWidth;
+        case SetWidth:
+            int newWidth;
             cout << "Введите новую ширину: ";
             cin >> newWidth;
+            if (newHeight < 0)
+            {
+                cout << "Ширина не может быть отрицательной. Попробуйте снова." << endl;
+                break;
+            }
             myRectangle.setWidth(newWidth);
-            cout << "Ширина изменена на " << myRectangle.getWidth() << std::endl;
+            cout << "Ширина изменена на " << myRectangle.getWidth() << endl;
             break;
-            case 0:
-                return;
-            default:
-                cout << "Неверный выбор! Попробуйте снова." << std::endl;
+        case 0:
+            return;
+        default:
+            cout << "Неверный выбор! Попробуйте снова." << endl;
             break;
         }
     }
 }
 
 
-void mainMenu() {
-    while (true) {
-        std::cout << "Выберите фигуру:\n";
-        std::cout << "1 - Круг\n2 - Прямоугольник\n3 - Трапеция\n4 - Выход" << std::endl;
+void mainMenu()
+{
+    while (true)
+    {
+        cout << "Выберите фигуру:\n";
+        cout << "1 - Круг\n2 - Прямоугольник\n3 - Трапеция\n0 - Выход" << endl;
 
         int choice;
-        if (!(std::cin >> choice)) {
-            std::cin.clear();
-            std::cin.ignore(10000, '\n');
-            std::cout << "Ошибка! Введите число от 1 до 4." << std::endl;
-            continue;
-        }
+        cin >> choice;
 
         ShapeOption shape = static_cast<ShapeOption>(choice);
 
-        switch (shape) {
-            case ShapeOption::Circle:
-                mainCircleFunction();
+        switch (shape)
+        {
+        case ShapeOption::Circle:
+            mainCircleFunction();
             break;
-            case ShapeOption::Rectangle:
-                RectOption();
+        case ShapeOption::Rectangle:
+            RectOption();
 
             break;
-            case ShapeOption::Trapezoid:
-                std::cout << "Функция для трапеции в разработке.\n" << std::endl;
+        case ShapeOption::Trapezoid:
+            cout << "Функция для трапеции в разработке.\n" << endl;
             break;
-            case ShapeOption::Exit:
-                return;
-            default:
-                std::cout << "Неверный выбор! Попробуйте снова.\n" << std::endl;
+        case ShapeOption::Exit:
+            return;
+        default:
+            cout << "Неверный выбор! Попробуйте снова.\n" << endl;
+            break;
         }
     }
 }
 
 
-int main() {
+int main()
+{
     mainMenu();
     return 0;
 }
